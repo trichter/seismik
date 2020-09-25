d97acbe95f48cbd138cb09542d29eb6ff3c7ab26
trichter <tom.eulenfeld@uni-jena.de>
2020-09-25 11:26:47 +0200

    fix fwd2synpicks for different spreads

--- data_example/fwd2synpicks.py
+++ data_example/fwd2synpicks.py
@@ -1,14 +1,18 @@
 from glob import glob
 import numpy as np
+import seisutil as su
 
-
-globexpr = 'INV/FWD/fd*.synth.asc'
+globexpr = 'INV/FWD2/fd*.synth.asc'
 soufname = 'GEO/sou_xz.dat'
 recfname = 'GEO/rec_xz.dat'
+spredfname = 'GEO/spreads.txt'
 fname_out = 'synthetic_picks.txt'
 
 sou = np.loadtxt(soufname)[:,:2]
 rec = np.loadtxt(recfname)[:,:2]
+spreads = su.read_spreads(spredfname, verbose=True)
+spread_from_sp = {sp: spread for spread in spreads
+                  for sp in spread['shotpoints']}
 
 out = []
 
@@ -17,11 +21,12 @@ for fname in sorted(glob(globexpr)):
     assert picks[0, -1] == -1
     assert all(picks[1:, -1] == 1)
     picks = picks[:, [0, 3]]
-    srcind = np.nonzero(np.abs(picks[0, 0] - sou[:, 1]) < 0.1)[0][0]
-    srcnum = sou[srcind, 0]
-    recind = np.nonzero(np.abs(picks[1:, 0] - rec[:, 1, np.newaxis]) < 0.1)[0]
-    recnums = rec[recind, 0]
-    o = np.vstack([np.ones(len(recnums))*srcnum, recnums, picks[1:,1] / 1000])
-    out.append(o)
+    for srcind in np.nonzero(np.abs(picks[0, 0] - sou[:, 1]) < 0.1)[0]:
+        srcnum = sou[srcind, 0]
+        rec0 = spread_from_sp[srcnum]['channels'][0]
+        recind = np.nonzero(np.abs(picks[1:, 0] - rec[:, 1, np.newaxis]) < 0.1)[0]
+        recnums = rec[recind, 0] - rec0 + 1
+        o = np.vstack([np.ones(len(recnums))*srcnum, recnums, picks[1:,1] / 1000])
+        out.append(o)
 
 np.savetxt(fname_out, np.transpose(np.hstack(out)), fmt=('%d', '%d', '%.5f'), delimiter='  ')
