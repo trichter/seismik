4f9f8d6eb59a47fb0c8bec748192f739471bb2d3
trichter <tom.eulenfeld@uni-jena.de>
2020-09-25 10:10:39 +0200

    readable color_func

--- data_example/map.py
+++ data_example/map.py
@@ -4,7 +4,11 @@
 from seisgeo import plot_html
 
 def color_func(name):
-    return '#3186cc' if name[:2] in ('GP', 'TR') else 'violet' if name[:2] == 'EL' else 'green' if name[0] in 'EDOUVW' else 'red'
+    return (
+        '#3186cc' if name[:2] in ('GP', 'TR') else
+        'violet' if name[:2] == 'EL' else
+        'green' if name[0] in 'EDOUVW' else
+        'red')
 
 
 plot_html('GEO/geo_eubabrunn_tag3.txt', 'map.html', zone=(33, 'N'), delimiter=',', color_func=color_func)
