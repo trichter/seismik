7531349008156d8235dd8b422b17102d326c7afa
trichter <tom.eulenfeld@uni-jena.de>
2022-08-23 15:19:00 +0200

    clean up, display rms, correctly calc rms

--- py/seismodel.py
+++ py/seismodel.py
@@ -14,6 +14,14 @@ Upon start set terminal to "always on top" and enlarge matplotlib figure.
 Press h for help on hot keys.
 """
 
+import json
+import sys
+import matplotlib.pyplot as plt
+import numpy as np
+
+import seisutil as su
+from seispick import _try_read_file, load_picks, calc_backshots
+
 _key_doc = """
 h: print help for keys
 c: clear shot selction
@@ -28,16 +36,11 @@ I: start IPython session
 
 Click marker for info on picks and to select specific shots
 """
-import numpy as np
-import sys
-from seispick import _try_read_file, load_picks, calc_backshots
-import seisutil as su
-import json
-import matplotlib.pyplot as plt
 
 
 def convert_picks(picks, stuff, sc):
     ch2rec = stuff.ch2rec
+
     def spreadch2ch(sp, ch):
         return ch - 1 + stuff.spread_from_sp[sp]['channels'][0]
     sht, channel, pick = zip(
@@ -65,17 +68,13 @@ def convert_picks(picks, stuff, sc):
 def load_stuff(verbose=False):
     with open('seisconf.json') as f:
         sc = json.load(f)
-    shotpoints = _try_read_file(su.read_shotpoints, sc['info'] + 'shotpoints.txt', verbose=verbose)
-    spreads = _try_read_file(su.read_spreads, sc['info'] + 'spreads.txt', verbose=verbose)
+    shotpoints = _try_read_file(
+        su.read_shotpoints, sc['info'] + 'shotpoints.txt', verbose=verbose)
+    spreads = _try_read_file(
+        su.read_spreads, sc['info'] + 'spreads.txt', verbose=verbose)
     stuff = calc_backshots(shotpoints, spreads, verbose=verbose)
     picks = load_picks('picks1.txt')
     return convert_picks(picks, stuff, sc)
-
-
-def linear_fit(x, y):
-    A = np.transpose(np.vstack([x, np.ones(len(x))]))
-    m, c = np.linalg.lstsq(A, y, rcond=None)[0]
-    return m, c
 
 
 def linear_fit_through(point, x, y):
@@ -83,39 +82,49 @@ def linear_fit_through(point, x, y):
     A = np.transpose([x-x0])
     result = np.linalg.lstsq(A, y-y0, rcond=None)
     m = result[0][0]
-    try:
-        err = result[1][0]
-    except IndexError:
-        err = 0.
     c = y0 - m * x0
-    return (m, c), err
+    return (m, c)
 
 
 def fit_legs(x2, x, t):
     mcs = []
     x2 = np.hstack((0, x2, np.max(x)+1))
     point = (0, 0)
-    sum_err = 0.
     for i in range(len(x2)-1):
         ind = np.logical_and(x >= x2[i], x < x2[i+1])
-        mc, err = linear_fit_through(point, x[ind], t[ind])
-        sum_err += err
+        mc = linear_fit_through(point, x[ind], t[ind])
         point = (x2[i+1], x2[i+1] * mc[0] + mc[1])
         mcs.append(mc)
-    return mcs, x2, sum_err
+    return mcs, x2
 
 
-def func(x2, x, t):
+def linear_fit(x, y):
+    A = np.transpose(np.vstack([x, np.ones(len(x))]))
+    result = np.linalg.lstsq(A, y, rcond=None)
+    return result[0]
+
+
+def fit_points(x2, t2, x, t):
+    mcs = []
+    for i in range(len(x2)-1):
+        mc = linear_fit(x2[i:i+2], t2[i:i+2])
+        mcs.append(mc)
+    return mcs
+
+
+def error_func(x2, x, t):
     if any(x2[i] >= x2[i+1] for i in range(len(x2) - 1)):
         return 100000
-    _, _, sum_err = fit_legs(x2, x, t)
-    return sum_err
+    mcs, x2 = fit_legs(x2, x, t)
+    rms = calc_rms(mcs, x2, x, t)
+    return rms
+
 
 def fit_ndmodel(n, x, t):
     from scipy.optimize import brute
     assert n >= 2
     ranges = [(np.min(x)+1, np.max(x)-1) for _ in range(n-1)]
-    x2 = brute(func, ranges, args=(x, t), workers=-1)
+    x2 = brute(error_func, ranges, args=(x, t), workers=-1)
     return fit_legs(x2, x, t)
 
 
@@ -133,35 +142,47 @@ def tt2model(mcs):
         hn = (tau[n+1]-tau0) / 2 / term(v[n], v[n+1])
         h.append(hn)
     for i in range(N-1):
-        print(f'  depth up to {h[i]:.1f}m: v={v[i]/1000:.2f}km/s')
-    h2 = h[-1] if len(h)>0 else 0
-    print(f'  below       {h2:.1f}m: v={v[-1]/1000:.2f}km/s')
+        print(f'  <{h[i]:.1f}m: v={v[i]/1000:.2f}km/s')
+    h2 = h[-1] if len(h) > 0 else 0
+    print(f'  >{h2:.1f}m: v={v[-1]/1000:.2f}km/s\n')
     return h, v
+
+
+def calc_rms(mcs, x2, x, t):
+    residuals = []
+    for i in range(len(x2)-1):
+        ind = np.logical_and(x >= x2[i], x < x2[i+1])
+        m, c = mcs[i]
+        residuals.append(t[ind] - m * x[ind] - c)
+    # print(residuals)
+    # print(np.shape(residuals))
+    residuals = np.hstack(residuals)
+
+    rms = (np.mean(residuals ** 2)) ** 0.5
+    return rms
 
 
 def fit_model(x, t, n=None, ttpicks=None, mode=None):
     x = np.abs(x)
     if ttpicks and mode == 't':
-        mcs = []
         x2, t2 = zip(*ttpicks)
-        for i in range(len(x2)-1):
-            mc = linear_fit(x2[i:i+2], t2[i:i+2])
-            mcs.append(mc)
+        mcs = fit_points(x2, t2, x, t)
     elif ttpicks and mode == 'r':
         x2, _ = zip(*ttpicks)
-        mcs, x2, _ = fit_legs(x2, x, t)
+        mcs, x2 = fit_legs(x2, x, t)
     else:
         assert n is not None
         if n == 1:
-            mcs, x2, _ = fit_legs([], x, t)
+            mcs, x2 = fit_legs([], x, t)
         else:
-            mcs, x2, _ = fit_ndmodel(n, x, t)
+            mcs, x2 = fit_ndmodel(n, x, t)
     # plot travel time curve
     for i in range(len(x2)-1):
         plt.plot(x2[i:i+2], mcs[i][0]*np.array(x2[i:i+2])+mcs[i][1], 'C0')
     print(f'Fitted {len(mcs)}d model')
+    rms = calc_rms(mcs, x2, x, t) * 1000
+    print(f'RMS: {rms:.2f}ms')
     tt2model(mcs)
-
 
 
 def plot_and_show(n, x, t, twosides=False):
@@ -187,10 +208,13 @@ def plot_and_show(n, x, t, twosides=False):
               f'select shots {np.unique(n[ind])}\n'
               f'position {np.median(x[ind]):.1f}m\n'
               f'ttime    {np.median(t[ind]):.4f}s\n')
+        # for n0 in np.unique(n[ind]):
+        #     colors[n == n0] = 'm'
+        ind2 = np.logical_or.reduce([n == n0 for n0 in n[ind]])
         colors = np.array(['k'] * len(n))
-        for n0 in np.unique(n[ind]):
-            colors[n == n0] = 'm'
-        fit_data[:] = [x[ind], t[ind]]
+        colors[ind2] = 'm'
+        fit_data[:] = [x[ind2], t[ind2]]
+        print(fit_data)
         markers.set_color(colors)
         fig.canvas.draw()
 
@@ -232,7 +256,8 @@ def plot_and_show(n, x, t, twosides=False):
             print('Clear axis from tt picks, deselect shots')
             mode = None
             ax.clear()
-            markers = ax.scatter(x, t, color='k', marker='x', picker=True, alpha=0.5)
+            markers = ax.scatter(
+                x, t, color='k', marker='x', picker=True, alpha=0.5)
             fit_data[:] = [x, t]
         elif event.key == 'I':
             # Danger zone - program must be killed afterwards
